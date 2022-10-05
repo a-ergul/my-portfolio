34b6e4f0ccf42ba28cdabb031c95209d7a28ba87
aergul <alpyergul@gmail.com>
2022-10-05 23:00:11 +0300

    #2 Feat: Created Navbar

---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Navbar.scss";
+
+function Navbar() {
+    return (
+        <nav className="app_navbar">
+            <div className="app_navbar-logo">
+            aergul-
+            </div>
+            <ul className="app_navbar-link">
+                {["home", "about", "work", "skills", "contact"].map((item) => (
+                    <li className="app_flex p-text" key={`link-${item}`}>
+                        <div />
+                        <a href={`#${item}`}>{item}</a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
