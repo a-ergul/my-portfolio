c4822a345688f9311a6090246f306ea56a9d05aa
aergul <alpyergul@gmail.com>
2022-10-09 23:28:16 +0300

    #4 Feat: Created Social Media Container

--- frontend/src/components/header/SocialMedia.jsx
+++ frontend/src/components/header/SocialMedia.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 
-import {FaFacebook } from "react-icons/fa"
+import { BsTwitter } from "react-icons/bs";
+
+import {FaLinkedinIn,FaGithub } from "react-icons/fa"
 
 function SocialMedia() {
     return (
         <div className="app_social">
             <div>
-                <FaFacebook/>
+                <FaGithub onClick={()=>window.open('https://github.com/a-ergul',"_blank")}/>
             </div>
+            <div>
+                <FaLinkedinIn onClick={() => window.open('https://www.linkedin.com/in/alpayergul/', "_blank")}/>
+            </div>
+            <div>
+        <BsTwitter onClick={()=>window.open('https://twitter.com/alpayergul',"_blank")}/>
+      </div>
         </div>
     )
 }
