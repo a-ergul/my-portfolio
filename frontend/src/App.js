11bc0eaaf244a9f17981c1395ce5ef22696b33dc
aergul <alpyergul@gmail.com>
2022-10-11 17:30:00 +0300

    #6 Feat: Create About Page, Install Sanity

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,17 @@ import logo from './logo.svg';
 import './App.scss';
 import Navbar from './components/navbar/Navbar';
 import Header from './components/header/Header';
+import About from './components/about/About';
 
 function App() {
   return (
     <div className="app">
       <Navbar/>
       <Header/>
+      <About/>
     </div>
   );
 }
+
 
 export default App;
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Header} from './header/Header'
+export { default as About} from './about/About'
 export { default as Navbar} from './navbar/Navbar'
 export { default as NavigationDots} from './header/NavigationDots'
 export { default as SocialMedia } from './header/SocialMedia'
