be0eb715ca0a105b2a774683e64f8f4ccfec9a06
aergul <alpyergul@gmail.com>
2022-10-14 17:41:46 +0300

    #12 Feat: Created Navigation Dots

--- frontend/src/components/header/NavigationDots.jsx
+++ frontend/src/components/header/NavigationDots.jsx
@@ -3,12 +3,12 @@ import React from "react";
 function NavigationDots({ active }) {
     return (
         <div className="app_navigation">
-            {["home", "about", "work", "skills", "testimonials", "contact"].map((item, index) => (
+            {["home", "about", "skills"].map((item, index) => (
                 <a
                 href={`#${item}`}
                 key={item + index}
                 className="app_navigation-dot"
-                style={active === item ? {backgroundColor: "#313BAC"} : {}}
+                style={active === item ? {backgroundColor: "tomato"} : {}}
                 />
             ))}
         </div>
--- frontend/src/components/wrapper/Appwrap.js
+++ frontend/src/components/wrapper/Appwrap.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import NavigationDots from "../header/NavigationDots";
 import SocialMedia from "../header/SocialMedia";
 
